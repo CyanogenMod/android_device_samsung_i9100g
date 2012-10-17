cfe93ec1fdbc72f1af9282892982e1c2e4f048a5
Andrew Jiang <sam.andrew.jiang@gmail.com>
2012-10-16 23:31:45 -0400

    I9100G: Add ISO Support
    Original commit: https://github.com/CyanogenMod/android_device_samsung_galaxys2-common/commit/74f6e5616a0eedd686e4ea9d9f063bdb7ad4b4f
    
    Kanged from the galaxys2-common repo with ISO 50 removed as it
    doesn't seem to work on I9100G.
    
    Change-Id: Iea3c0dc3d0089899cfc420a7c298ed3eb1b4cc21

--- camerawrapper/CameraWrapper.cpp
+++ camerawrapper/CameraWrapper.cpp
@@ -99,8 +99,8 @@ static char * camera_fixup_getparams(int id, const char * settings)
     params.unflatten(android::String8(settings));
 
     // fix params here
-#warning "Reimplement KEY_SUPPORTED_ISO_MODES"
-//    params.set(android::CameraParameters::KEY_SUPPORTED_ISO_MODES, iso_values[id]);
+
+    params.set("iso-values", iso_values[id]);
 
     android::String8 strParams = params.flatten();
     char *ret = strdup(strParams.string());
@@ -114,20 +114,21 @@ char * camera_fixup_setparams(int id, const char * settings)
     android::CameraParameters params;
     params.unflatten(android::String8(settings));
 
-/*
+
     // fix params here
-    if(params.get("iso")) {
-        const char* isoMode = params.get(android::CameraParameters::KEY_ISO_MODE);
-        if(strcmp(isoMode, "ISO100") == 0)
-            params.set(android::CameraParameters::KEY_ISO_MODE, "100");
-        else if(strcmp(isoMode, "ISO200") == 0)
-            params.set(android::CameraParameters::KEY_ISO_MODE, "200");
-        else if(strcmp(isoMode, "ISO400") == 0)
-            params.set(android::CameraParameters::KEY_ISO_MODE, "400");
-        else if(strcmp(isoMode, "ISO800") == 0)
-            params.set(android::CameraParameters::KEY_ISO_MODE, "800");
+
+    const char* isoMode = params.get("iso");
+    if(isoMode) {
+        if(!strcmp(isoMode, "ISO100"))
+            params.set("iso", "100");
+        else if(!strcmp(isoMode, "ISO200"))
+            params.set("iso", "200");
+        else if(!strcmp(isoMode, "ISO400"))
+            params.set("iso", "400");
+        else if(!strcmp(isoMode, "ISO800"))
+            params.set("iso", "800");
     }
-*/
+
     android::String8 strParams = params.flatten();
     char *ret = strdup(strParams.string());
 
@@ -144,7 +145,7 @@ int camera_set_preview_window(struct camera_device * device,
 {
     ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device, (uintptr_t)(((wrapper_camera_device_t*)device)->vendor));
 
-    if(!device)
+    if(!device || !window)
         return -EINVAL;
 
     return VENDOR_CALL(device, set_preview_window, window);
