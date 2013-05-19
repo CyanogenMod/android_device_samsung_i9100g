d70d8a14abcc9f470a3398430952e566f8843e77
Andrew Jiang <sam.andrew.jiang@gmail.com>
2013-05-19 09:16:18 -0400

    I9100G: Enable exposure settings on front camera
    Change-Id: I648a693bd18ba27c3e36e573c727bf9affb275a9

--- camerawrapper/CameraWrapper.cpp
+++ camerawrapper/CameraWrapper.cpp
@@ -100,13 +100,9 @@ static char * camera_fixup_getparams(int id, const char * settings)
 
     // fix params here
     params.set(android::CameraParameters::KEY_SUPPORTED_ISO_MODES, iso_values[id]);
-
-    /* Back camera */
-    if(id == 0) {
-        params.set(android::CameraParameters::KEY_EXPOSURE_COMPENSATION_STEP, "1.0");
-        params.set(android::CameraParameters::KEY_MIN_EXPOSURE_COMPENSATION, "-3");
-        params.set(android::CameraParameters::KEY_MAX_EXPOSURE_COMPENSATION, "3");
-    }
+    params.set(android::CameraParameters::KEY_EXPOSURE_COMPENSATION_STEP, "1.0");
+    params.set(android::CameraParameters::KEY_MIN_EXPOSURE_COMPENSATION, "-3");
+    params.set(android::CameraParameters::KEY_MAX_EXPOSURE_COMPENSATION, "3");
 
     android::String8 strParams = params.flatten();
     char *ret = strdup(strParams.string());
