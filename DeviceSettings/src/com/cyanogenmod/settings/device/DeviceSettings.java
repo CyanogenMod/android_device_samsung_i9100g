6f97a7b001c276ef96e5b589eff4f4f5bf8cb9c2
Andrew Jiang <sam.andrew.jiang@gmail.com>
2013-06-09 14:27:18 -0400

    I9100G: Add Content Adaptive Backlight Control & Panel Gamma Control
    Change-Id: I0e80d868e95e66c21bcbe8f6a772bed664bac015

--- DeviceSettings/src/com/cyanogenmod/settings/device/DeviceSettings.java
+++ DeviceSettings/src/com/cyanogenmod/settings/device/DeviceSettings.java
@@ -36,6 +36,8 @@ public class DeviceSettings extends FragmentActivity {
     public static final String SHARED_PREFERENCES_BASENAME = "com.cyanogenmod.settings.device";
     public static final String ACTION_UPDATE_PREFERENCES = "com.cyanogenmod.settings.device.UPDATE";
     public static final String KEY_HSPA = "hspa";
+    public static final String KEY_CABC = "cabc";
+    public static final String KEY_PANEL_GAMMA = "panel_gamma";
     public static final String KEY_TOUCHSCREEN_SENSITIVITY = "touchscreen_sensitivity";
     public static final String KEY_TOUCHKEY_LED_MODE = "touchkey_led_mode";
     public static final String KEY_TOUCHKEY_TIMEOUT = "touchkey_timeout";
