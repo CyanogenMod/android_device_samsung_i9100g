9a41903c6c70378a35dbee6b8674fc3988fa1e62
Andrew Jiang <sam.andrew.jiang@gmail.com>
2013-11-10 07:17:05 -0500

    I9100G: Fix Audio effects
    Actually use CM's DSP libraries for effects and actually copy audio_effects.conf into the
    system.
    
    Change-Id: I33cfa279e717a90ac3441d03cf71fa3abedd4704

--- i9100g.mk
+++ i9100g.mk
@@ -35,7 +35,7 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_effects.conf \
+    $(LOCAL_PATH)/configs/audio_effects.conf:system/etc/audio_effects.conf \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
 
 # GPS
