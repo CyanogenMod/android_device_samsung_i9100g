5cda48f429a639670029bf25c4f19ec505231e38
Ryuinferno <ryuinferno.xda@gmail.com>
2014-12-16 15:10:05 +0100

    i9100g: More updates to media codecs
    Change-Id: Ib69dd14458beee54330c924795f30d158aa6468a

--- i9100g.mk
+++ i9100g.mk
@@ -58,7 +58,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Media profiles
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml
 
 # Packages
 PRODUCT_PACKAGES += \
